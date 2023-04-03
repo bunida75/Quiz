7826a46576c7758973bacd9fa5ea1d513d645efc
unknown <buntida@gmail.com>
2023-04-03 10:31:46 +0700

    pang

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program {
+static void Main(string[] args);
+ user input;
+ user input = 0;
+ int count = 0;
+
+ User input = input("End");
+Console.Write("End" ,"type" to finish );
+string user input = Console.Readline();
+if (user input == "End"){
+
+}
+ break
+ {
+    bool isParsrd = 
+ }
+
+}
+    
